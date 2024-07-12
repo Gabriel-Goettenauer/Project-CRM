d9c01ce51c03901e2875d1090c1a70363005d8a1
CodeAcademy <gabrielgoettenauer9@gmail.com>
2024-07-12 13:13:57 -0300

    exclusao de itens desnecessarios

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -18,31 +18,11 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-use App\Http\Controllers\PersonalAccessTokenController;
-
-use App\Http\Controllers\LoginController;
-
-Route::get('/logins', [LoginController::class, 'index'])->name('logins.index');
-Route::post('/logins/create', [LoginController::class, 'create'])->name('logins.create');
-Route::post('/register', [LoginController::class, 'store'])->name('logins.store');
-Route::get('/logins/{id}', [LoginController::class, 'show'])->name('logins.show');
-Route::get('/logins/{id}/edit', [LoginController::class, 'edit'])->name('logins.edit');
-Route::put('/logins/{id}', [LoginController::class, 'update'])->name('logins.update');
-Route::delete('/logins/{id}', [LoginController::class, 'destroy'])->name('logins.destroy');
-
-
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-
-
-Route::get('password/reset', [PasswordController::class, 'showLinkRequestForm'])->name('password.request');
-Route::post('password/email', [PasswordController::class, 'sendResetLinkEmail'])->name('password.email');
-Route::get('password/reset/{token}', [PasswordController::class, 'showResetForm'])->name('password.reset');
-Route::post('password/reset', [PasswordController::class, 'reset'])->name('password.update');
-Route::post('password/update', [PasswordController::class, 'update'])->middleware('auth')->name('password.update.authenticated');
 
 
 Route::get('/funnels', [FunnelController::class, 'index'])->name('funnels.index');
