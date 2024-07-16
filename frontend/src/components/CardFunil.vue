2c6dd4cb59b333a56a8129fa7c7e4ff2ca846959
Gustavo da Silva Moscal <gustavosilvam85@gmail.com>
2024-07-16 16:02:14 -0300

    Dashboard Funis feitos e Com Offcanvas

--- frontend/src/components/CardFunil.vue
+++ frontend/src/components/CardFunil.vue
@@ -2,7 +2,11 @@
     <div>
         <div class="card m-3" style="width: 18rem;">
             <div class="card-body">
-                <h5 class="card-title">Card title</h5>
+                <div class="d-flex justify-content-between card-info">
+                    <h5 class="card-title">Card title</h5>
+                    <i class="bi bi-trash-fill" @click="s"></i>
+                </div>
+
                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
             </div>
         </div>
@@ -15,6 +19,22 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 
+.card{
+    background-color:#FFFFFF;
+    color: #373753;
+    font-family: 'CerebriSansProRegular';
+    border: 1px solid #E1E9F4;
+
+}
+.card-text{
+    color:#677C92;
+}
+.card-info i{
+    color:#677C92;
+}
+i:hover{
+    color: #FF3F3E;
+}
 </style>
